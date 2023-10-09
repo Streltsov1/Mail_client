cb07e9ef7c6415af2f62b49304bf657d984418f2
Strel_ba60@student.itstep.org <Strel_ba60@student.itstep.org>
2023-10-09 17:59:06 +0300

    ewqe

--- Mail_client/Window1.xaml.cs
+++ Mail_client/Window1.xaml.cs
@@ -38,26 +38,46 @@ namespace Mail_client
             Num = num;
             FileName = new List<string>();
             Folders.ItemsSource = folder;
+            //using (var client = new ImapClient())
+            //{
+            //    client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
 
+            //    client.Authenticate(Mail, Password);
+            //    foreach (var item in client.GetFolders(client.PersonalNamespaces[0]))
+            //    {
+            //        Folder folder1 = new Folder();
+            //        folder1.FolderName = item.Name;
+            //        foreach (var uid in client.Inbox.Search(SearchQuery.All))
+            //        {
+            //            var m = client.Inbox.GetMessage(uid);
+            //            folder1.mail1.Add(new Mail() { MailName = m.Subject });
+            //        }
+            //        folder.Add(folder1);
+            //    }
+            //}
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             using (var client = new ImapClient())
             {
-                client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                client.Connect("imap.ukr.net", 993, SecureSocketOptions.SslOnConnect);
 
                 client.Authenticate(Mail, Password);
-
                 foreach (var item in client.GetFolders(client.PersonalNamespaces[0]))
                 {
-                    Folder folder = new Folder();
-                    folder.FolderName = item.Name;
-                    foreach (var uid in client.Inbox.Search(SearchQuery.All))
-                    {
-                        var m = client.Inbox.GetMessage(uid);
-                        Console.WriteLine($"Mail: {m.Subject} - {new string(m.TextBody.Take(10).ToArray())}...");
-                    }
+                    Folder folder1 = new Folder();
+                    folder1.FolderName = item.Name;
+                    //var folders = client.GetFolder(SpecialFolder.All);
+                    //folders.Open(FolderAccess.ReadWrite);
+                    //IList<UniqueId> uids = folders.Search(SearchQuery.All);
+
+                    //foreach (var i in uids)
+                    //{
+                    //    MimeMessage message = folders.GetMessage(i);
+                    //    folder1.mail1.Add(new Mail() { MailName = message.Subject });
+                    //}
+                    folder.Add(folder1);
                 }
             }
         }
